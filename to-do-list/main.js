8407875e1651c39e2cb4127f52b391c0009eb390
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-10-10 16:24:44 +0900

    Feat: todo 삭제 기능 구현

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -6,26 +6,35 @@ const items = document.querySelectorAll(".todos__items");
 
 // 리스트 추가 함수
 const onAdd = (index) => {
-  const inputValue = inputs[index].value;
+  const input = inputs[index];
   // console.log(index);
-  if(!inputValue) return;
+  if(!input.value) return;
   // console.log(index);
   const li = document.createElement("li");
   const p = document.createElement("p");
   const deleteBtn = document.createElement("button");
 
+  // class 지정
   li.setAttribute("class", "todos__item");
   p.setAttribute("class", "todos__name");
   deleteBtn.setAttribute("class", "todos__delete");
-
+  
+  // 텍스트
   deleteBtn.innerText = "❌";
-  p.innerText = inputValue;
+  p.innerText = input.value;
 
+  // 삭제 기능
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  // 추가
   items[index].appendChild(li);
   li.appendChild(p);
   li.appendChild(deleteBtn);
 
-  inputValue = "";
+  // input 창 초기화
+  input.value = "";
 }
 
 // 추가 버튼 클릭
@@ -41,6 +50,3 @@ inputs.forEach((input, index) => addEventListener("keyup", event => {
 }));
 
 
-// deleteBtn.addEventListener("click", () => {
-  
-// });
