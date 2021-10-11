02665d4c6f2a093829acbeda6409ef05e01fc937
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-10-11 14:55:40 +0900

    Feat: Dropdown - changing Active

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -2,13 +2,25 @@
 
 const navSorting = document.querySelector(".nav__sorting");
 const navDropdown = document.querySelector(".nav__dropdown");
+const navActiveSorting = document.querySelector(".nav__sorting--active");
 
 const onDropdown = () => {
   navDropdown.classList.toggle("visible");  
 }
 
+const changeActive = (e) => {
+  navActiveSorting.innerText = e.target.innerText;
+
+  Array.from(navDropdown.children)
+    .filter((list) => list.classList.contains("active"))[0]
+    .classList.remove("active");
+
+  e.target.classList.add("active");
+}
+
 const init = () => {
   navSorting.addEventListener("click", onDropdown);
+  navDropdown.addEventListener("click", changeActive);
 }
 
 init();
