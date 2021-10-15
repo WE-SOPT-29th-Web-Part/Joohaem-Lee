e6f588f5dd37bbc94eecab18aabd26e16c9cd0bc
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-10-15 18:03:06 +0900

    Feat: 이미지 슬라이더 구현 완료

--- velog-clone/slider.js
+++ velog-clone/slider.js
@@ -4,22 +4,25 @@ const slideList = document.querySelector('.main__slide-list');
 const slideBtnPrev = document.querySelector('.main__slide-btn-prev');
 const slideBtnNext = document.querySelector('.main__slide-btn-next');
 
-const slideWidth = document.querySelector('.main__slide-content').offsetWidth;
 const slideLen = 5;
 let slideIndex = 0;
 
 const goPrev = () => {
+  const slideWidth = document.querySelector('.main__slide-content').offsetWidth;
   if (slideIndex >= 0) {
-    slideList.style.transform = "translate3d(-" + (slideWidth * slideIndex) + "px, 0px, 0px)";
+    slideList.style.transform = `translate3d(-${(slideWidth * slideIndex)}px, 0px, 0px)`;
     slideIndex--;
   }
+  console.log(slideWidth, slideIndex);
 };
 
 const goNext = () => {
-  if (slideIndex <= slideLen - 1) {
-    slideList.style.transform = "translate3d(-" + (slideWidth * (slideIndex + 1)) + "px, 0px, 0px)";
+  const slideWidth = document.querySelector('.main__slide-content').offsetWidth;
+  if (slideIndex < slideLen - 1) {
+    slideList.style.transform = `translate3d(-${(slideWidth * (slideIndex + 1))}px, 0px, 0px)`;
     slideIndex++;
   }
+  console.log(slideLen, slideIndex);
 };
 
 const initSlide = () => {
