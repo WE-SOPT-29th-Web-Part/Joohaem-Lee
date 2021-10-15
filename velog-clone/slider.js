04465cc3a5db4ff534bdc0066f72d4bf26d64b56
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-10-15 21:06:31 +0900

    Feat: 이미지 슬라이더 max indexing 추가

--- velog-clone/slider.js
+++ velog-clone/slider.js
@@ -4,25 +4,34 @@ const slideList = document.querySelector('.main__slide-list');
 const slideBtnPrev = document.querySelector('.main__slide-btn-prev');
 const slideBtnNext = document.querySelector('.main__slide-btn-next');
 
-const slideLen = 5;
+const slideLen = document.querySelectorAll(".main__slide-content").length;
 let slideIndex = 0;
 
 const goPrev = () => {
-  if (slideIndex < 0) return;
+  if (slideIndex <= 0) return;
 
   const slideWidth = document.querySelector('.main__slide-content').offsetWidth;
   
-  slideList.style.transform = `translate3d(-${(slideWidth * slideIndex)}px, 0px, 0px)`;
   slideIndex--;
+  slideList.style.transform = `translate3d(-${(slideWidth * slideIndex)}px, 0px, 0px)`;
 };
 
+const whatMaxIndex = () => {
+  const browserWidth = window.innerWidth;
+
+  if(browserWidth < 768) return slideLen - 1;
+  else if(browserWidth < 1024) return slideLen - 2;
+  else return slideLen - 3;
+}
+
 const goNext = () => {
-  if (slideIndex > slideLen - 2) return;
+  if (slideIndex >= whatMaxIndex()) return;
 
   const slideWidth = document.querySelector('.main__slide-content').offsetWidth;
   
-  slideList.style.transform = `translate3d(-${(slideWidth * (slideIndex + 1))}px, 0px, 0px)`;
   slideIndex++;
+  slideList.style.transform = `translate3d(-${(slideWidth * (slideIndex))}px, 0px, 0px)`;
+  console.log(slideIndex);
 };
 
 const initSlide = () => {
