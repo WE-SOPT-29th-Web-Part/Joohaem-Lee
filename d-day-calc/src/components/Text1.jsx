3ce172dca3314a64f8677174ec4039c3705b2c82
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-10-24 01:31:13 +0900

    Feat: Calc 기능 구현

--- d-day-calc/src/components/Text1.jsx
+++ d-day-calc/src/components/Text1.jsx
@@ -10,7 +10,7 @@ const Input = styled.input`
   width: 10%;
   height: 2em;
   border: 1px solid #e4e4e4;
-  padding-left: 10px;
+  padding-left: 5px;
   margin-right: 0.5em;
 `;
 const ResultDate = styled.p`
@@ -21,25 +21,34 @@ const Text1 = ({ year, month, date }) => {
   const [inputValue, setInputValue] = useState("");
   const [resultDate, setResultDate] = useState("yyyy년 mm월 dd일");
 
+  const handleDday = (e) => {
+    if (!e.target.value) {
+      setInputValue("");
+      setResultDate(`yyyy년 mm월 dd일`);
+      return;
+    }
+    setInputValue(e.target.value);
+    printDate(e.target.value);
+  };
+
   const printDate = useCallback(
     (value) => {
+      if (value === undefined || value === "") {
+        setResultDate("yyyy년 mm월 dd일");
+        return;
+      }
       const tempDate = new Date();
       tempDate.setFullYear(year);
       tempDate.setMonth(Number(month) - 1);
       tempDate.setDate(Number(date) + Number(value) - 1);
-
-      const result = `${tempDate.getFullYear()}년 ${
-        tempDate.getMonth() + 1
-      }월 ${tempDate.getDate()}일`;
-      setResultDate(result);
+      setResultDate(
+        `${tempDate.getFullYear()}년 ${
+          Number(tempDate.getMonth()) + 1
+        }월 ${tempDate.getDate()}일`
+      );
     },
     [year, month, date]
   );
-
-  const handleDday = (e) => {
-    setInputValue(e.target.value);
-    printDate(inputValue);
-  };
 
   useEffect(() => {
     printDate(inputValue);
@@ -47,7 +56,8 @@ const Text1 = ({ year, month, date }) => {
 
   return (
     <Wrapper>
-      <Input type="text" onChange={handleDday} />일 째 되는 날은?
+      <Input type="text" value={inputValue} onChange={handleDday} />일 째 되는
+      날은?
       <ResultDate>{resultDate}</ResultDate>
     </Wrapper>
   );
--- d-day-calc/src/components/Text2.jsx
+++ d-day-calc/src/components/Text2.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.section`
@@ -10,22 +10,55 @@ const Input = styled.input`
   width: 10%;
   height: 2em;
   border: 1px solid #e4e4e4;
-  padding-left: 10px;
+  padding-left: 5px;
   margin-right: 0.5em;
 `;
 const ResultDate = styled.p`
   text-align: right;
 `;
 
-// dday 계산하기!
-
 const Text2 = ({ year, month, date }) => {
+  const [resultDate, setResultDate] = useState("yyyy년 mm월 dd일");
+  const [inputValue, setInputValue] = useState();
+  const handleDday = (e) => {
+    if (!e.target.value) {
+      setInputValue("");
+      setResultDate(`yyyy년 mm월 dd일`);
+      return;
+    }
+    setInputValue(e.target.value);
+    printDate(e.target.value);
+  };
+
+  const printDate = useCallback(
+    (value) => {
+      if (value === undefined || value === "") {
+        setResultDate("yyyy년 mm월 dd일");
+        return;
+      }
+      const tempDate = new Date();
+      tempDate.setFullYear(year);
+      tempDate.setMonth(Number(month) - 1);
+      tempDate.setDate(Number(date) - Number(value));
+      setResultDate(
+        `${tempDate.getFullYear()}년 ${
+          Number(tempDate.getMonth()) + 1
+        }월 ${tempDate.getDate()}일`
+      );
+    },
+    [year, month, date]
+  );
+
+  useEffect(() => {
+    printDate(inputValue);
+  }, [inputValue, printDate]);
+
   return (
     <Wrapper>
       D-
-      <Input type="text" />
+      <Input type="text" value={inputValue} onChange={handleDday} />
       는?
-      <ResultDate>yyyy년 mm월 dd일</ResultDate>
+      <ResultDate>{resultDate}</ResultDate>
     </Wrapper>
   );
 };
