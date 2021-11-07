21a357c0e47b394d4a6aec7a29bacc56dff08dd1
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-11-07 17:03:29 +0900

    Feat: 건영이 배포 강의 (오타수정2)

--- react-velog-clone/src/App.js
+++ react-velog-clone/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Write from "./pages/Write";
 
