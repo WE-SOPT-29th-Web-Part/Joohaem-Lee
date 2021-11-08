12313c9914dfcb301b310c537117604af89a24f4
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-11-08 22:58:35 +0900

    Feat: pending, no result UI

--- github-profile-finder/src/components/PendingResult.jsx
+++ github-profile-finder/src/components/PendingResult.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  margin-top: 1rem;
+  background-color: rgb(44, 48, 53);
+  width: 25rem;
+  border-radius: 20px;
+  position: relative;
+`;
 
 const PendingResult = () => {
-  return <div>Pending...</div>;
+  return <Card>Pending...</Card>;
 };
 
 export default PendingResult;
