2627221df8b68c3744c628dda515e4c21dd83243
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-11-12 15:22:58 +0900

    Feat: ArticleBody 구현

--- react-velog-clone/src/components/Write/ArticleBody.jsx
+++ react-velog-clone/src/components/Write/ArticleBody.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
 const ArticleBody = ({ setArticleData }) => {
   const handleChange = (e) => {
+    if (e.target.value.length > 10) {
+      e.target.value = e.target.value.substring(0, 10);
+      e.target.style.color = "red";
+      return;
+    }
+    e.target.style.color = "inherit";
     setArticleData((articleData) => ({
       ...articleData,
       body: e.target.value,
     }));
   };
   return (
-    <textarea
-      placeholder="내용을 입력하세요"
+    <Textarea
+      placeholder="당신의 이야기를 적어보세요..."
       onChange={handleChange}
-    ></textarea>
+    ></Textarea>
   );
 };
+
+const Textarea = styled.textarea`
+  all: unset;
+  width: 100%;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: rgb(33, 37, 41);
+  &::placeholder {
+    font-style: italic;
+  }
+`;
 
 export default ArticleBody;
