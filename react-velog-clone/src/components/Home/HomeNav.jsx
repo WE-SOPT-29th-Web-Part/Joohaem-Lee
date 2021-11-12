0d1aa40245f63479161586f07e534e2a3cfa591d
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-11-12 17:25:28 +0900

    Feat: HomeNav 메뉴 애니메이션 구현ㅠㅠ

--- react-velog-clone/src/components/Home/HomeNav.jsx
+++ react-velog-clone/src/components/Home/HomeNav.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { Outlet } from "react-router";
+
+const HomeNav = () => {
+  const { pathname } = useLocation();
+  const [navIdx, setNavIdx] = useState(0);
+
+  return (
+    <>
+      <NavWrapper>
+        <Items>
+          <Item selected={pathname === "/"} onClick={() => setNavIdx(0)}>
+            <StyledLink to="">글</StyledLink>
+          </Item>
+          <Item selected={pathname === "/series"} onClick={() => setNavIdx(1)}>
+            <StyledLink to="/series">시리즈</StyledLink>
+          </Item>
+        </Items>
+        <NavUnderline position={navIdx} />
+      </NavWrapper>
+      <Outlet />
+    </>
+  );
+};
+
+const NavWrapper = styled.nav`
+  position: relative;
+  width: 16rem;
+  margin: 0 auto;
+`;
+
+const NavUnderline = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 3px;
+  width: 8rem;
+  background-color: var(--main-green-color);
+  transition: transform 0.3s;
+  transform: translateX(${({ position }) => position * 8}rem);
+`;
+
+// -------------- 위까지 HomeNav box ---------------
 
 const Items = styled.ul`
   display: flex;
@@ -17,8 +57,6 @@ const Item = styled.li`
   font-weight: 700;
   color: ${({ selected }) =>
     selected ? "var(--main-green-color)" : "var(--main-gray-color)"};
-  border-bottom: 3px solid
-    ${({ selected }) => (selected ? "var(--main-green-color)" : "none")};
 `;
 
 const StyledLink = styled(Link)`
@@ -28,25 +66,7 @@ const StyledLink = styled(Link)`
   display: flex;
   justify-content: center;
   align-items: center;
+  color: inherit;
 `;
-
-const HomeNav = () => {
-  const { pathname } = useLocation();
-  return (
-    <>
-      <nav>
-        <Items>
-          <Item selected={pathname === "/"}>
-            <StyledLink to="">글</StyledLink>
-          </Item>
-          <Item selected={pathname === "/series"}>
-            <StyledLink to="/series">시리즈</StyledLink>
-          </Item>
-        </Items>
-      </nav>
-      <Outlet />
-    </>
-  );
-};
 
 export default HomeNav;
